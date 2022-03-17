b47084dbe15ee6ebffcfa172317d615dbe4e754b
Tim Heurich <tim.heurich@idealo.de>
2022-03-17 17:16:09 +0100

    PR Remark

--- Formula/go-aws-sso.rb
+++ Formula/go-aws-sso.rb
@@ -22,6 +22,6 @@ class GoAwsSso < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/go-aws-sso", "-v"
   end
 end
