65663351a6c1cce1d0ee0a6cf94b0e410aae323f
Tim Heurich <16006309+theurichde@users.noreply.github.com>
2022-03-23 19:33:50 +0000

    go-aws-sso: update 0.5.0 bottle.

--- Formula/go-aws-sso.rb
+++ Formula/go-aws-sso.rb
@@ -6,7 +6,10 @@ class GoAwsSso < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/theurichde/homebrew-go-aws-sso/releases/download/go-aws-sso-0.4.0"
+    root_url "https://github.com/theurichde/homebrew-go-aws-sso/releases/download/go-aws-sso-0.5.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "29bcc77cec331a7b1de094d0030bf6cbad1f360823dd7d7068d0487a937bd2bb"
+    sha256 cellar: :any_skip_relocation, catalina:     "3d6b7e1dd1521e2580a37bbd11db78baf35a8389ef37bf696d30acb78ca91463"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9c351a33c9995132c0e0d1df97b069826e9c96c5214f60e38d46ab84f7c47f7a"
   end
 
   depends_on "go" => :build
