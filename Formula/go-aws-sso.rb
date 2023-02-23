62dcd3b0b40d064b477bfb6724676c175952c367
Tim Heurich <16006309+theurichde@users.noreply.github.com>
2023-02-23 21:49:59 +0000

    go-aws-sso: update 1.2.0 bottle.

--- Formula/go-aws-sso.rb
+++ Formula/go-aws-sso.rb
@@ -7,6 +7,10 @@ class GoAwsSso < Formula
 
   bottle do
     root_url "https://github.com/theurichde/homebrew-go-aws-sso/releases/download/go-aws-sso-1.2.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, monterey:     "c3282e12d5c92df564b5871bf3629c677438cbf5ecbe5f5d9cf257e2ef7ad23c"
+    sha256 cellar: :any_skip_relocation, big_sur:      "0ad11912b16bf00b38e7d0bb596cea7c5a5c0f29f2cfeb49df957d198834f40c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f22e16440c4b30658e185f5fc697abc4282261c5bde01adda1ab25519ecdac9e"
   end
 
   depends_on "go" => :build
